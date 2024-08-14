0ac71795c5a1edd32def4b6052ed7d0f5c701ca0
DMikaia <faeldan03@gmail.com>
2024-08-14 18:20:01 +0300

    Finished basic config

---
+++ tailwind.config.mjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      mobile: "375px",
+      tablet: "640px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
+    fontFamily: {
+      "RedHatText-400": "var(--red-hat-text-regular)",
+      "RedHatText-600": "var(--red-hat-text-semibold)",
+      "RedHatText-700": "var(--red-hat-text-bold)",
+    },
+    extend: {
+      colors: {
+        red: "hsl(var(--red))",
+        green: "hsl(var(--green))",
+        rose: {
+          50: "hsl(var(--rose-50))",
+          100: "hsl(var(--rose-100))",
+          300: "hsl(var(--rose-300))",
+          400: "hsl(var(--rose-400))",
+          500: "hsl(var(--rose-500))",
+          900: "hsl(var(--rose-900))",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
